92cef6d020e79ecb5d1c903f28f1c6f45a1f8cd8
Siyabonga Mbokazi <darelmbokazi@gmail.com>
2025-05-23 16:42:38 +0200

    adding vervel.json file

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import userRoutes from './routes/userRoutes'
 import taskRoutes from './routes/taskRoutes'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
@@ -11,12 +12,18 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
+app.use(cookieParser())
+
 app.use(
   cors({
     origin: ['https://user-task-management-five.vercel.app'],
     credentials: true,
   })
 )
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Vercel!')
+})
 
 app.use('/api', userRoutes)
 app.use('/api', taskRoutes)
