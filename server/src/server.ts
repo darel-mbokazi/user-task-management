6c2ad7a32dca2d82b85b3ee55847f4241b039294
Siyabonga Mbokazi <darelmbokazi@gmail.com>
2025-05-23 16:19:52 +0200

    adding frontend url

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,24 @@ import userRoutes from './routes/userRoutes'
 import taskRoutes from './routes/taskRoutes'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const app = express()
-const PORT = 3000
+const port = process.env.PORT
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: ['user-task-management-five.vercel.app'],
+    credentials: true,
+  })
+)
 
 app.use('/api', userRoutes)
 app.use('/api', taskRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port: ${PORT}`)
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`)
 })
